86a43381c4812ff928671b942dc2df93fae634ea
Fernando Andres Poblete <andrespbt14@hotmail.com>
2023-03-08 21:22:08 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export const App = () => {
+  return (
+    <div className="App">
+      <h1 className="font-bold text-3xl">Elige un color</h1>
+      <form>{/* aqui deberias escribir tu codigo */}</form>
+    </div>
+  );
+};
